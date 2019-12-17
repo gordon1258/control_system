12a7fd3e19c66e9b91400dd6d5494d75cd75029e
Gordon Hsu <gordon1258@gmail.com>
2019-12-16 23:05:31 -0800

    Created the control_system project

---
+++ control_system/control_system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  control_system
+//
+//  Created by Li-Cheng Hsu on 12/16/19.
+//  Copyright © 2019 Li-Cheng Hsu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
